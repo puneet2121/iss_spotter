49d216d023c49a176cc8964cbe85fd449d0b7f05
puneet2121 <bedipuneet21@gmail.com>
2022-06-09 21:18:15 +0000

    Fix bug

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const {fetchISSFlyOverTimes} = require('./iss');
 const {nextISSTimesForMyLocation} = require('./iss');
 const myChoords =  { latitude: '49.69919967651367', longitude: '-112.8233413696289' };
 const mycallback = (error,ip) => {
-  if(error){
+  if (error) {
     console.log("It did not work!",error);
     return;
   }
-  console.log('It worked! Retured IP:',ip)
+  console.log('It worked! Retured IP:',ip);
   return;
-}
+};
 fetchMyIP(mycallback);
 
 fetchCoordsByIP('68.144.212.2', (error, coordinates) => {
@@ -22,27 +22,27 @@ fetchCoordsByIP('68.144.212.2', (error, coordinates) => {
   console.log('It worked! Returned coordinates:' , coordinates);
 });
 fetchISSFlyOverTimes(myChoords,(error,time) => {
-  if(error){
+  if (error) {
     console.log("It did'nt work!",error);
     return;
   }
-  console.log('It worked! Retured IP:',time)
+  console.log('It worked! Retured IP:',time);
   return;
-  });
-  const printPassTimes = function(passTimes) {
-    for (const passes of passTimes) {
-      const datetime = new Date(0);
-      datetime.setUTCSeconds(passes.risetime);
-      const duration = passes.duration;
-      console.log(`Next  pass at ${datetime} for ${duration} seconds!`);
-    }
-  };
+});
+const printPassTimes = function(passTimes) {
+  for (const passes of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(passes.risetime);
+    const duration = passes.duration;
+    console.log(`Next  pass at ${datetime} for ${duration} seconds!`);
+  }
+};
   
-  nextISSTimesForMyLocation((error, passTimes) => {
-    if (error) {
-      return console.log("It didn't work", error);
-    }
-    printPassTimes(passTimes);
-  });
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work", error);
+  }
+  printPassTimes(passTimes);
+});
 
 
--- iss.js
+++ iss.js
@@ -1,55 +1,55 @@
 const request = require('request');
 // const arg = process.argv[2];
 // request(url, (error, response, desc) => {
-  const fetchCoordsByIP = function(ip,callback){
-    request(`https://freegeoip.app/json/${ip}`,(error,response,body) => {
-      if (error) {
-        callback(error, null);
-        return;
-      }
-      if (response.statusCode !== 200) {
-        callback(Error(`Status Code of ${response.statusCode} when fetching Coordinates for IP add: ${body}`), null);
-        return;
-      }
-      const { latitude, longitude } = JSON.parse(body);
-      callback(null, { latitude, longitude });
-      return
-    });
-  };
-
-const fetchISSFlyOverTimes = function(coords,callback){
-  request((`https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`),(error,response,body) =>{
-    if(error){
-      // console.log('fetch my IP error',error)
-      callback(error,null)
-      return
-  }
-  if (response.statusCode !== 200) {
-    callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+const fetchCoordsByIP = function(ip,callback) {
+  request(`https://freegeoip.app/json/${ip}`,(error,response,body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code of ${response.statusCode} when fetching Coordinates for IP add: ${body}`), null);
+      return;
+    }
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
     return;
-  }
+  });
+};
 
-  const passes = JSON.parse(body).response;
-  callback(null, passes);
-});
+const fetchISSFlyOverTimes = function(coords,callback) {
+  request((`https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`),(error,response,body) =>{
+    if (error) {
+
+      callback(error,null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  });
 };
   
 
 
-const fetchMyIP = function(givenCallBack){
+const fetchMyIP = function(givenCallBack) {
 // this is where i will do api call
   request('https://api.ipify.org?format=json',(error,response,body) => {
     if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      givenCallBack(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
       return;
     }
-    if(error){
+    if (error) {
       // console.log('fetch my IP error',error)
-      givenCallBack(error,null)
-      return
+      givenCallBack(error,null);
+      return;
     }
     const data = JSON.parse(body);
-    givenCallBack(null,data);
+    givenCallBack(null,data.ip);
 
   });
 };
